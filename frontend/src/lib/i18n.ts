97f08c293e197a76bce0b6324de80985c6cb373f
i-junaidkhan <i.galoisfield@gmail.com>
2026-03-18 10:40:36 +0900

    Initial commit

---
+++ frontend/src/lib/i18n.ts
@@ -0,0 +1,25 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import { resources } from './locales'
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: 'ko-KR',
+    fallbackLng: 'ko-KR',
+    interpolation: {
+      escapeValue: false, // react already safes from xss
+    },
+    react: {
+      useSuspense: false,
+    },
+    detection: {
+      order: ['localStorage', 'navigator'],
+      caches: ['localStorage'],
+    },
+  })
+
+export default i18n
